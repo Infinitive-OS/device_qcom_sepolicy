LineageOS/android_device_qcom_sepolicy

sepolicy: Include CM+QCOM SEpolicy
Change-Id: I6b7f1716f9850e93145d9d379df1ffc0d97e19a2

sepolicy.mk
@@ -5,3 +5,5 @@ BOARD_SEPOLICY_DIRS := \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+-include vendor/cm/sepolicy/qcom/sepolicy.mk

Infinitive-OS/device_qcom_sepolicy

sepolicy.mk
@@ -5,3 +5,5 @@ BOARD_SEPOLICY_DIRS := \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+-include vendor/io/sepolicy/qcom/sepolicy.mk